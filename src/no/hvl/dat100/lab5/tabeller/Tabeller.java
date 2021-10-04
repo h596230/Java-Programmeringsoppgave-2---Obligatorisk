99d106191888eba5eef794767b6843d3066bba9f
h596230 <89384745+h596230@users.noreply.github.com>
2021-10-04 17:07:53 +0200

    first commit

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -1,64 +1,118 @@
 package no.hvl.dat100.lab5.tabeller;
 
-public class Tabeller {
+import java.util.Arrays;
 
+public class Tabeller {
+	public static void main(String[] args) {
+		System.out.print("xD");
+	}
 	// a)
 	public static void skrivUt(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
-
+		System.out.print("[");
+        for (int i = 0; i < tabell.length; i++) {
+            if (i < tabell.length - 1) {
+                System.out.print(tabell[i] + ",");
+            } else {
+                System.out.print(tabell[i]);
+            }
+        }
+        System.out.print("]");
+        System.out.println();
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		String str = "[";
+        for (int i = 0; i < tabell.length; i++) {
+            if (i < tabell.length - 1) {
+                str += tabell[i] + ",";
+            } else {
+                str += tabell[i];
+            }
+        }
+        str += "]";
+        return str;		
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
+        int sum = 0;
+        // for løkke
+        /*for(int i = 0;i < tabell.length; i++){
+            sum += tab[i];
+        }*/
+        // while løkke
+        /*int i = 0;
+        while (i < tabell.length){
+                sum += tab[i];
+            i++;
+        }*/
+        // utvidet for-løkke (ligner litt på forEach in js)
+        for (int i : tabell) {
+            sum += i;
+        }
+        return sum;
 
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
+        boolean finnes = false;
+        for (int i : tabell) {
+            if (i == tall) {
+                finnes = true;
+            }
+        }
+        System.out.println();
+        return finnes;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
-
+        int posisjon = -1;
+        for (int index = 0; index < tabell.length; index++) {
+            if (tabell[index] == tall) {
+                posisjon = index;
+            }
+        }
+        return posisjon;
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("reverser ikke implementert");
-		
+		int[] newArr = new int[tabell.length];
+        int reverseIndex = tabell.length - 1;
+        for(int i = 0;i < tabell.length; i++){
+            newArr[i] = tabell[reverseIndex];
+            reverseIndex--;
+        }
+        return newArr;
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
+		if(tabell == null || tabell.length <= 1){
+	        return true;
+	    }
+	    for(int i = 0; i < tabell.length - 1; i++){
+	        if(tabell[i] > tabell[i + 1]) {
+	            return false;
+	        }
+	    }
+	    return true;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-
-		// TODO
-		throw new UnsupportedOperationException("settSammen ikke implementert");
+		int[] newArr = new int[tabell1.length + tabell2.length];
+        int indexHolder = tabell1.length;
+        for(int i = 0; i < tabell1.length; i++){
+            newArr[i] = tabell1[i];
+        }
+        for (int i = 0; i < tabell2.length; i++){
+            newArr[indexHolder] = tabell2[i];
+            indexHolder++;
+        }
+        return newArr;
 	}
 }
--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -4,47 +4,58 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+        for(int[] m : matrise){
+        	for(int i : m) {
+        		System.out.print(i);
+        	}
+        	System.out.println();
+        }
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		String str = "";
+        for(int[] m : matrise){
+            for(int i = 0; i< m.length; i++){
+                str += m[i] + " ";
+            }
+            str += "\n";
+        }
+        return str;
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
-	
+		int[][] newM = new int[matrise.length][];
+        for (int i = 0; i < matrise.length; i++) {
+            int[] midlertidig = new int[matrise[i].length];
+            for (int j = 0; j < matrise[i].length; j++) {
+                midlertidig[j] = tall * matrise[i][j];
+            }
+            newM[i] = midlertidig;
+        }
+        return newM;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
-	}
-	
-	// e)
-	public static int[][] speile(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("speile ikke implementert");
-	
-	}
-
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("multipliser ikke implementert");
-	
-	}
+		if(a.length == b.length){
+            for(int i  = 0; i < a.length; i++){
+                if(a[i].length == b[i].length){
+                    for(int j = 0; j < a[i].length; j++){
+                        if(a[i][j] !=b[i][j]){
+                            return false;
+                        }
+                    }
+                }else {
+                    return false;
+                }
+            }
+        //
+        }else {
+            return false;
+        }
+    return true;
+    }
+	// fikk ikke e og f til
 }
